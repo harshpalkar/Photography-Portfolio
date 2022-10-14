6e3395fa7331d0ba06f4dfa529f6948e171c9300
Harsh Palkar <explorewitharsh@outlook.com>
2022-10-14 13:53:10 +0530

    master

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,26 +1,25 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+    myFunction();
+};
 
 var topnav = document.getElementById("topnav");
 var sticky = topnav.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    topnav.classList.add("sticky")
-  } else {
-    topnav.classList.remove("sticky");
-  }
+    if (window.pageYOffset >= sticky) {
+        topnav.classList.add("sticky");
+    } else {
+        topnav.classList.remove("sticky");
+    }
 }
 AOS.init();
-window.addEventListener('load', AOS.refresh)
-document.onreadystatechange = function () {
-
-if (document.readyState == 'complete') 
-{
-    AOS.refresh({
-      offset:120,
-      delay:20,
-      duration=4000,
-    });
-}
-
-}
+window.addEventListener("load", AOS.refresh);
+document.onreadystatechange = function() {
+    if (document.readyState == "complete") {
+        AOS.refresh({
+            offset: 120,
+            delay: 20,
+            duration: 4000,
+        });
+    }
+};
